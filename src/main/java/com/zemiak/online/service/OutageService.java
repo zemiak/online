ded90945cfd4691f724d2a1f23ade221c041900d
zemiak <zemiak@gmail.com>
2015-05-24 19:47:48 +0200

    Time comparing fixes

--- src/main/java/com/zemiak/online/service/OutageService.java
+++ src/main/java/com/zemiak/online/service/OutageService.java
@@ -1,6 +1,7 @@
 package com.zemiak.online.service;
 
 import com.zemiak.online.model.Outage;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.ejb.Stateless;
@@ -10,6 +11,7 @@ import javax.persistence.PersistenceContext;
 
 @Stateless
 public class OutageService {
+    private static final int ONE_DAY = 1000 * 3600 * 24;
 
     @PersistenceContext
     private EntityManager em;
@@ -20,20 +22,24 @@ public class OutageService {
 
     public List<Outage> findLastMonth(Long id) {
         return findByProtectedSystem(id).stream()
-                .filter(system -> system.getStart().getTime() > (1000 * 3600 * 24 * 31))
+                .filter(system -> system.getStart().getTime() > getTimeDaysAgo(33))
                 .collect(Collectors.toList());
     }
 
     public List<Outage> findLastYear(Long id) {
         return findByProtectedSystem(id).stream()
-                .filter(system -> system.getStart().getTime() > (1000 * 3600 * 24 * 366))
+                .filter(system -> system.getStart().getTime() > getTimeDaysAgo(366))
                 .collect(Collectors.toList());
     }
 
     public List<Outage> findLastDayByEnd(Long id) {
         return findByProtectedSystem(id).stream()
                 .filter(system -> null != system.getEnd())
-                .filter(system -> system.getEnd().getTime() > (1000 * 3600 * 24))
+                .filter(system -> system.getEnd().getTime() > getTimeDaysAgo(1))
                 .collect(Collectors.toList());
+    }
+
+    private long getTimeDaysAgo(int days) {
+        return (new Date()).getTime() - (ONE_DAY * days);
     }
 }
